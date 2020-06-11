6d157fb1ec68b9060d44b66cca1ea3fa318845f6
zongzhang <jingchang_007@163.com>
2020-06-11 14:49:05 +0800

    add CharsCode

--- src/main/java/com/github/zongzhang/lol/json/utils/CharsCode.java
+++ src/main/java/com/github/zongzhang/lol/json/utils/CharsCode.java
@@ -4,11 +4,16 @@ package com.github.zongzhang.lol.json.utils;
  * @since 1.0
  */
 public class CharsCode {
+    //space
     public static final int space = 0x0020; //半角空格
     public static final int space_1 = 0x3000; //全角空格
     public static final int space_2 = 0x00A0; //不间断空
+    //linefeed  \n
+    public static final int lineFeed = 0x000a;
+    //carriage return \r
+    public static final int carriageReturn = 0x000d;
+    //horizontal tab
     public static final int tab = 0x0009;
-
 
     public static final int dot = 0x002e;
 
@@ -31,6 +36,17 @@ public class CharsCode {
 
     public static final int doubleQuotes = 0x0022;
 
+    //{} Opening and closing braces
+    public static final int openBraces = 0x007b;
+    public static final int closeBraces = 0x007d;
+
+    //() Opening and closing parentheses
+    public static final int openParentheses = 0x0028;
+    public static final int closeParentheses = 0x0029;
+
+    //[] Opening and closing brackets
+    public static final int openBrackets = 0x005b;
+    public static final int closeBrackets = 0x005d;
 
     public static boolean isDigit(int ch) {
         return ch >= _0 && ch <= _9;
@@ -40,6 +56,8 @@ public class CharsCode {
         return ch == space
                 || ch == space_1
                 || ch == space_2
+                || ch == lineFeed
+                || ch == carriageReturn
                 || ch == tab;
     }
 }
